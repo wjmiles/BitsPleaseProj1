123c8ea194bdc172b86f502263ccb9930f4711c7
jcpratt <jcpratt@asu.edu>
2019-02-14 11:52:27 -0700

    added C# accountManaget folder. The createAccount.html in the accountManaget\accountManager folder connectsto the database.

---
+++ accountManager/accountManager/AccountServices.asmx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+using System.Data;
+
+namespace accountManager
+{
+    /// <summary>
+    /// Summary description for AccountServices
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class AccountServices : System.Web.Services.WebService
+    {
+        [WebMethod(EnableSession = true)]
+        public string NewUserAccount(string screenName, string email, string firstName, string lastName, string password)
+        {
+            string sqlConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+
+            string sqlSelect = "INSERT INTO `users` (`ScreenName`, `Email`, `FirstName`, `LastName`, `Password`) " + 
+                               "VALUES(@sNameValue, @emailValue, @fNameValue, @lNameValue, @passwordValue);";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectionString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@sNameValue", HttpUtility.UrlDecode(screenName));
+            sqlCommand.Parameters.AddWithValue("@emailValue", HttpUtility.UrlDecode(email));
+            sqlCommand.Parameters.AddWithValue("@fNameValue", HttpUtility.UrlDecode(firstName));
+            sqlCommand.Parameters.AddWithValue("@lNameValue", HttpUtility.UrlDecode(lastName));
+            sqlCommand.Parameters.AddWithValue("@passwordValue", HttpUtility.UrlDecode(password));
+
+            sqlConnection.Open();
+            try
+            {
+                int id = Convert.ToInt32(sqlCommand.ExecuteScalar());
+            }
+            catch (Exception e) {
+            }
+            sqlConnection.Close();
+
+            return email;
+        }
+
+        [WebMethod(EnableSession = true)]
+        public bool logOn(string email, string password)
+        {
+            bool success = false;
+
+            string sqlConnectionSring = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+
+            string sqlSelect = "SELECT UserID, AdminT/F FROM users WHERE email=@emailValue and password=@passwordValue";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectionSring);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@emailValue", HttpUtility.UrlDecode(email));
+            sqlCommand.Parameters.AddWithValue("@passwordValue", HttpUtility.UrlDecode(password));
+
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            DataTable sqlDt = new DataTable();
+            sqlDa.Fill(sqlDt);
+            if (sqlDt.Rows.Count > 0)
+            {
+                Session["UserID"] = sqlDt.Rows[0]["UserID"];
+                Session["AdminT/F"] = sqlDt.Rows[0]["AdminT/F"];
+                success = true;
+            }
+
+            return success;
+        }
+
+        [WebMethod(EnableSession = true)]
+        public bool LogOff()
+        {
+            Session.Abandon();
+            return true;
+        }
+
+        [WebMethod]
+        public int NumberOfUsers()
+        {
+            string sqlConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            string sqlSelect = "SELECT * from users";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectionString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            DataTable sqlDt = new DataTable();
+            sqlDa.Fill(sqlDt);
+            return sqlDt.Rows.Count;
+        }
+    }
+}
---
+++ accountManager/accountManager/createAccountScript.js
@@ -0,0 +1,93 @@
+﻿
+function storeAccountInfo() {
+
+    //altert text needs to be changes
+
+    var screenName, userName, fName, lName, email, password;
+
+    screenName = document.getElementById("screenName").value;
+    userName = document.getElementById("email").value;
+    fName = document.getElementById("fName").value;
+    lName = document.getElementById("lName").value;
+    email = document.getElementById("email").value;
+
+    if (document.getElementById("password").value ===
+        document.getElementById("passwordRepeat").value)
+    {
+        password = document.getElementById("password").value;
+    }
+
+    //more validation should be done before calling 'addUserToDB' funcrion
+    //email format
+    //handle empty string different than undefined - allert message
+
+    if (screenName === "" ||
+        userName === "" ||
+        fName === "" ||
+        lName === "" ||
+        email === "" ||
+        password === "" ||
+        password === undefined)
+    {
+        document.getElementById("password").value = "";
+        document.getElementById("passwordRepeat").value = "";
+        alert("No"); 
+    }
+    else {
+        addUserToDB(screenName, userName, fName, lName, email, password);
+        document.getElementById("screenName").value = "";
+        document.getElementById("fName").value = "";
+        document.getElementById("lName").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+        document.getElementById("passwordRepeat").value = "";
+    }
+    /*
+    //Test
+    console.log("screenName: " + screenName);
+    console.log("userName: " + userName);
+    console.log("fName: " + fName);
+    console.log("lName: " + lName);
+    console.log("email: " + email);
+    console.log("password: " + password);
+    */
+	/*
+	var screenName = screenName.text;
+  	var userName = email.text;
+  	var fName = fName.text;
+  	var lName = lName.text;
+  	var email = email.text;
+  
+  	if (password.text == passwordRepeat.text){
+    	var password = password.text;
+  	}
+	*/
+}
+
+function addUserToDB(screenName, userName, fName, lName, email, password) {
+
+    //altert text needs to be changes
+
+    var webMethod = "AccountServices.asmx/NewUserAccount";
+    var parameters = "{\"screenName\":\"" + encodeURI(screenName) +
+                      "\",\"email\":\"" + encodeURI(email) +
+                      "\",\"firstName\":\"" + encodeURI(fName) +
+                      "\",\"lastName\":\"" + encodeURI(lName) +
+                      "\",\"password\":\"" + encodeURI(password) + "\"}";
+
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        data: parameters,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            alert("Might of worked! :)");
+            //console.log(parameters);
+        },
+        error: function (e) {
+            alert("Probably didn't work :(");
+            //console.log(parameters);
+        }
+    });
+}
