9fa1182bd947761b85f87f932952fac4c2c9f880
wjmiles <wjmiles@asu.edu>
2019-02-14 15:24:30 -0700

    Deleted old createAccount files

---
+++ Scripts/createAccountScript.js
@@ -0,0 +1,12 @@
+function storeAccountInfo()
+{
+  var screenName = screenName.text;
+  var userName = email.text;
+  var fName = fName.text;
+  var lName = lName.text;
+  var email = email.text;
+  
+  if (password.text == passwordRepeat.text){
+    var password = password.text;
+  }
+}
